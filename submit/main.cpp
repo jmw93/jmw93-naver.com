42e0b5b9fa046dc790d8068c527f50f4aea14f83
minwooJeong <jmw93@naver.com>
2020-02-23 16:32:40 +0900

    c++first commit

---
+++ submit/main.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <queue>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int s[13];
+int n ;
+int check[13]; //12 까지
+void dfs(int num, int cnt){
+    if(cnt == 6){
+        for(int i=1;i<=n;i++){
+            if(check[i]){
+                cout<< s[i]<<" ";
+            }
+        }
+        cout<<endl;
+        return;
+    } // 갯수만큼 다돌았을때
+    
+    for(int i =num+1; i<=n;i++){
+        if(!check[i]){
+            check[i] = true;
+            dfs(i,cnt+1);
+            check[i] = false; // 백트래킹
+        }
+    }
+}
+
+int main(int argc,const char * argv[]){
+    while(1){
+        cin>>n;
+        if(n==0)
+            break;
+        for(int i=1;i<=n;i++){
+        cin>>s[i];
+        }
+        dfs(0,0);
+        cout<<endl;
+        }
+    }
+
+
+
