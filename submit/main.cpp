d35fd8e1c21b5172a148200f313f29a733995907
minwooJeong <jmw93@naver.com>
2020-04-05 18:06:32 +0900

    올바른괄호

--- submit/main.cpp
+++ submit/main.cpp
@@ -1,46 +1,39 @@
-#include <stdio.h>
-#include <queue>
-#include <string>
-#include <vector>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <string>
 #include <cmath>
+#include <cstring>
+#include <queue>
+#include <cstdlib>
+#include <string.h>
+
 using namespace std;
-int s[13];
-int n ;
-int check[13]; //12 까지
-void dfs(int num, int cnt){
-    if(cnt == 6){
-        for(int i=1;i<=n;i++){
-            if(check[i]){
-                cout<< s[i]<<" ";
+stack<char> st;
+
+bool solution(string s){
+    
+    for(int i = 0 ; i<s.size();i++){
+        if(s[i] == '('){
+            st.push(s[i]);
+        }else{ // 닫는괄호일때
+            if(st.empty() || st.top() != '('){//스택이비거나 , top이 )가 아니면!
+                return false;
+            }else{ // 스택이 정상적으로 빠질 때.
+                st.pop();
             }
         }
-        cout<<endl;
-        return;
-    } // 갯수만큼 다돌았을때
-    
-    for(int i =num+1; i<=n;i++){
-        if(!check[i]){
-            check[i] = true;
-            dfs(i,cnt+1);
-            check[i] = false; // 백트래킹
-        }
+        
     }
+    if(!st.empty()) return false; //스택이 비어있지않으면 ,
+    else return true;
 }
-
-int main(int argc,const char * argv[]){
-    while(1){
-        cin>>n;
-        if(n==0)
-            break;
-        for(int i=1;i<=n;i++){
-        cin>>s[i];
-        }
-        dfs(0,0);
-        cout<<endl;
-        }
-    cout <<"중간업로드를 위한 테스트";
-    }
-
-
-
+int main(int argc, const char* argv[]){
+    string str;
+    getline(cin,str);
+    int ret = solution(str);
+    cout <<ret;
+}
